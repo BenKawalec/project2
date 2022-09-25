b985093fa2247f987e1526fe62afdf4771ba04ad
Ben Kawalec <bkawalec10@gmail.com>
2022-09-25 18:47:51 -0400

    adadwa

--- config/connection.js
+++ config/connection.js
@@ -5,23 +5,22 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // setting up sequelize object using sequelize instructor imported from the sequelize node module
-const sequelize = new Sequelize(
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
-);
-// let cleardb = {
-//     HOST: "us-cdbr-east-06.cleardb.net",
-//     USER: "bc5ad3e98ffdb4",
-//     PASSWORD: "270d519a",
-//     DB: "heroku_2216c2050836347"
-//   };
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 
 //exporting our newly created sequelize object
 module.exports = sequelize;
