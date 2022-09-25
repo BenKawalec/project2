eb24e4769b22e2f38a1d03132bb36300f529aeb3
Ben Kawalec <bkawalec10@gmail.com>
2022-09-25 15:21:47 -0400

    update2

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,7 @@ const sequelize = new Sequelize(
 
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-    }
+    process.env.DB_PASSWORD
 );
 
 //exporting our newly created sequelize object
