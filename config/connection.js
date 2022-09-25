aaa3a00e85427d010490df25cbd7051b4d629610
Ben Kawalec <bkawalec10@gmail.com>
2022-09-25 15:52:41 -0400

    4

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,23 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // setting up sequelize object using sequelize instructor imported from the sequelize node module
-module.exports = {
+const sequelize = new Sequelize(
+
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+);
+let cleardb = {
     HOST: "us-cdbr-east-06.cleardb.net",
     USER: "bc5ad3e98ffdb4",
     PASSWORD: "270d519a",
     DB: "heroku_2216c2050836347"
   };
 
+//exporting our newly created sequelize object
+module.exports = sequelize + cleardb;
